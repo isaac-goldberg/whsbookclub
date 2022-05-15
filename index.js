c2884b8cda8be54305872a36547e8250f6faa493
Isaac Goldberg <isaacg0ldb3rg@gmail.com>
2022-05-14 22:02:45 -0700

    fixed assets and views dir refs

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const rootRoutes = require("./src/routes/root-routes");
 
 // app settings
 app.engine("html", mustacheExpress());
-app.set("views", `${__dirname}/views`);
+app.set("views", `${__dirname}/src/views`);
 app.set("view engine", "html")
 
 // app view directories
-app.use(express.static(`${__dirname}/assets`));
-app.locals.basedir = `${__dirname}/assets`;
+app.use(express.static(`${__dirname}/src/assets`));
+app.locals.basedir = `${__dirname}/src/assets`;
 
 // use routers
 app.use("/", rootRoutes);
